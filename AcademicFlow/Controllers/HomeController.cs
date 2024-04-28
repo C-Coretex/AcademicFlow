775fbd7b18d616b7c36af52578a648e7a9b9b422
V Sh <valerash2@gmail.com>
2024-04-28 21:25:30 +0300

    init infrastructure

--- AcademicFlow/Controllers/HomeController.cs
+++ AcademicFlow/Controllers/HomeController.cs
@@ -7,7 +7,6 @@ namespace AcademicFlow.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
--- AcademicFlow/Program.cs
+++ AcademicFlow/Program.cs
@@ -1,3 +1,5 @@
+using AcademicFlow.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
