da0dd82ae5cc99351495adabacbaf05ecd869363
V Sh <valerash2@gmail.com>
2024-04-30 20:09:06 +0300

    delete junk files

--- AcademicFlow.Infrastructure.Contracts/IRepositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using AcademicFlow.Domain.Helpers.Base;
-using AcademicFlow.Domain.Entities;
-
-namespace AcademicFlow.Infrastructure.Contracts.IRepositories
-{
-    public interface IUserRepository: RepositoryBase<AcademicFlowDbContext, User>
-    {
-    }
-}
