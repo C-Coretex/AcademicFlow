4e539996f22aaad015762766448acb0cb9756476
Jelizaveta <jz21055@edu.lu.lv>
2024-05-29 20:42:09 +0300

    Bug fix

---
+++ AcademicFlow.Migrations/Migrations/20240529171843_FixCourseIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicFlow.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCourseIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Course_PublicId",
+                table: "Course");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_PublicId",
+                table: "Course",
+                column: "PublicId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Course_PublicId",
+                table: "Course");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Course_PublicId",
+                table: "Course",
+                column: "PublicId");
+        }
+    }
+}
