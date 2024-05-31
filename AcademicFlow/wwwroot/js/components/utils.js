43fa10c1835e24adb184ebc73bda791668cc59ed
sakuraaah <niktro30@gmail.com>
2024-05-31 17:18:27 +0300

    move assignment table func to utils

--- AcademicFlow/wwwroot/js/components/utils.js
+++ AcademicFlow/wwwroot/js/components/utils.js
@@ -8,6 +8,48 @@ export function toggleObjectVisibility($object, state) {//State is false -> hide
     }
 }
 
+export async function addAssignmentTable(parent, courseId) {
+    const assignments = await $.ajax({
+        url: '/api/Assignment/GetAllAssignmentsForCourse',
+        method: "GET",
+        dataType: "json",
+        data: { 
+            courseId: courseId
+        }
+    });
+
+    if (!assignments?.length) {
+        parent.append('<p>No assignments available.</p>');
+        return;
+    }
+
+    const table = $('<table class="table">');
+
+    table.append($(`
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Deadline</th>
+            </tr>
+        </thead>
+    `));
+
+    const tbody = $('<tbody>');
+
+    $.each(assignments, function(_, assignment) {
+        const tr = $('<tr>');
+
+        tr.append($(`<th scope="row">${assignment.id}</th>`))
+        tr.append($(`<td><a href="/Home/Assignment/${assignment.id}" class="text-success">${assignment.assignmentName}</a></td>`))
+        tr.append($(`<td class="text-muted">${assignment.assignmentDescription}</td>`))
+        tr.append($(`<th>${assignment.deadline}</th>`))
+
+        tbody.append(tr);
+    });
+}
+
 export async function getCurrentUser() {
     return new Promise((resolve, reject) => {
         $.ajax({
