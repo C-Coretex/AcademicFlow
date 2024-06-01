d0440ee8f50456da762adf11102109448227cd35
sakuraaah <niktro30@gmail.com>
2024-06-02 00:38:14 +0300

    update user manager tests

--- AcademicFlow.Tests/UserManagerTests.cs
+++ AcademicFlow.Tests/UserManagerTests.cs
@@ -29,8 +29,11 @@ namespace AcademicFlow.Tests
         [Fact]
         public async Task GetUsers_ShouldReturnUsers()
         {
+            // Arrange
+            var controllerUrl = "";
+
             // Act
-            var result = await _userManager.GetUsers().ToListAsync();
+            var result = await _userManager.GetUsers(controllerUrl).ToListAsync();
 
             // Assert
             Assert.NotNull(result);
@@ -82,7 +85,16 @@ namespace AcademicFlow.Tests
                     PersonalCode = "12345-67890",
                     Email = "test@email.com",
                     PhoneNumber = "+37125477836",
-                    Age = 23
+                    Age = 23,
+                    IsDeleted = false,
+                    UserCredentials = new ()
+                    {
+                        Id = 1,
+                        Username = "Test 1",
+                        PasswordHash = "Āsdasdasdasdasdad",
+                        Salt = "asdasdasd",
+                        SecurityKey = "gfdgdg",
+                    }
                 },
                 new() {
                     Id = 2,
@@ -90,18 +102,38 @@ namespace AcademicFlow.Tests
                     Surname = "Surname 2",
                     PersonalCode = "52386-67110",
                     Email = "test2@email.com",
+                    IsDeleted = false,
+                    UserCredentials = new ()
+                    {
+                        Id = 2,
+                        Username = "Test 2",
+                        PasswordHash = "Āsdasdasdasdasdad",
+                        Salt = "asdasdasd",
+                        SecurityKey = "gfdgdg",
+                    }
                 },
                 new() {
                     Id = 3,
                     Name = "Name 3",
                     Surname = "Surname 3",
                     PersonalCode = "17345-67854",
+                    IsDeleted = false,
+                    UserCredentials = new ()
+                    {
+                        Id = 3,
+                        Username = "Test 3",
+                        PasswordHash = "Āsdasdasdasdasdad",
+                        Salt = "asdasdasd",
+                        SecurityKey = "gfdgdg",
+                    }
                 },
             };
 
+            var asNoTracking = true;
+
             //mock IAsyncQueryable/IAsyncEnumerable
             var usersMock = _users.AsQueryable().BuildMock();
-            _userRepositoryMock.Setup(x => x.GetAll()).Returns(usersMock);
+            _userRepositoryMock.Setup(x => x.GetAll(asNoTracking)).Returns(usersMock);
             _userRepositoryMock.Setup(x => x.AddAsync(It.IsAny<User>(), It.IsAny<bool>())).Callback<User, bool>((x, y) =>
             {
                 x.Id = _users.Max(u => u.Id) + 1;
