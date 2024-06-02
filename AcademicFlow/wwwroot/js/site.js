a013f542367256113c28d7debbd6b549cd63f2b9
sakuraaah <niktro30@gmail.com>
2024-06-02 20:18:22 +0300

    test fix + import fix

--- AcademicFlow/wwwroot/js/site.js
+++ AcademicFlow/wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-import { checkUserPermissionsLevel, getURLbyUserRole, renderHeaderLinks, getCurrentUser, proceedLogout } from "./components/utils.js";
+import { checkUserPermissionsLevel, getURLbyUserRole, renderHeaderLinks, getCurrentUser, proceedLogout } from "./components/utils";
 
 
 $(document).ready(async function () {
@@ -13,13 +13,13 @@ $(document).ready(async function () {
         const userDataTab = $('<li class="nav-item js-users-tab d-none ml-4">');
         userDataTab.append($(`<a class="nav-link text-dark">Hello, ${userData.name} ${userData.surname}!</a>`))
 
-        const logoutTab = $('<li class="nav-item js-users-tab d-none">');
-        const logoutLink = $('<a class="nav-link text-dark cursor-pointer">Logout</a>');
-        logoutLink.click(proceedLogout);
-        logoutTab.append(logoutLink);
+        // const logoutTab = $('<li class="nav-item js-users-tab d-none">');
+        // const logoutLink = $('<a class="nav-link text-dark cursor-pointer">Logout</a>');
+        // logoutLink.click(proceedLogout);
+        // logoutTab.append(logoutLink);
 
         headerNav.append(userDataTab);
-        headerNav.append(logoutTab);
+        // headerNav.append(logoutTab);
 
         let userRole = await checkUserPermissionsLevel(userData.roles);
         //redirectUserWithoutAccess(userRole);
