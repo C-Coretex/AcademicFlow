3c47e0c51b45f5157699c87407da33682eb9c3bb
Jelizaveta <jz21055@edu.lu.lv>
2024-06-02 22:42:32 +0300

    Add userRole validation

---
+++ AcademicFlow.Managers/Managers/EducationBaseManager.cs
@@ -0,0 +1,19 @@
+﻿using AcademicFlow.Domain.Contracts.Enums;
+using AcademicFlow.Domain.Entities;
+using AutoMapper;
+
+namespace AcademicFlow.Managers.Managers
+{
+    public class EducationBaseManager(IMapper mapper) : BaseManager(mapper)
+    {
+        public static string? UserRoleValidation(IQueryable<User> userQuery, HashSet<int>? userIds, RolesEnum role)
+        {
+            var notValidUsers = userQuery.Where(x => userIds.Contains(x.Id) && !x.UserRoles.Any(x => x.Role == role)).Select(x => x.Id).AsEnumerable();
+            if (notValidUsers != null && notValidUsers.Any())
+            {
+                return $"Users with ids [{string.Join(", ", notValidUsers)}] have no {role} role. No one user is assigned";
+            }
+            return null;
+        }
+    }
+}
