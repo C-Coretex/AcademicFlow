24e347e111c638c0742d1b1a7c554167cb014911
V Sh <valerash2@gmail.com>
2024-06-03 22:55:07 +0300

    include fix

--- AcademicFlow.Domain/Services/UserService.cs
+++ AcademicFlow.Domain/Services/UserService.cs
@@ -48,6 +48,8 @@ namespace AcademicFlow.Domain.Services
         public async Task<User?> GetUserByIdWithAssignments(int userId)
         {
             var user = await _userRepository.GetAll().Include(x => x.AssignmentTasks)
+                                                     .ThenInclude(x => x.Course)
+                                                     .Include(x => x.AssignmentTasks)
                                                      .ThenInclude(x => x.AssignmentEntries)
                                                      .ThenInclude(x => x.AssignmentGrade)
                                                      .FirstOrDefaultAsync(u => u.Id == userId);
